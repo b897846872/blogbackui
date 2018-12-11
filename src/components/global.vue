6ccdfb0733ec16830dd744530870460b8f0357db
897846872@qq.com <897846872@qq.com>
2018-12-11 23:02:45 +0800

    tinymce上传图片

--- src/components/global.vue
+++ src/components/global.vue
@@ -17,9 +17,33 @@ const editorInit = {
   toolbar: 'bold italic underline strikethrough | fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | undo redo | link unlink image code | removeformat',
   branding: false,
   selector: 'textarea',  // change this value according to your html
-  images_upload_url: 'postAcceptor.php',
+  images_upload_url: '/blog/tabArticle/uploadImage',
   images_upload_base_path: '/some/basepath',
-  images_upload_credentials: true
+  images_upload_credentials: true,
+  images_upload_handler: function (blobInfo, success, failure) {
+    var xhr, formData;
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.open('POST', '/blog/tabArticle/uploadImage');
+    xhr.onload = function() {
+      var json;
+
+      if (xhr.status != 200) {
+      failure('HTTP Error: ' + xhr.status);
+      return;
+      }
+      json = JSON.parse(xhr.responseText);
+
+      if (!json || typeof json.location != 'string') {
+      // failure('Invalid JSON: ' + xhr.responseText);
+      success(json.data);
+      return;
+      }
+    };
+    formData = new FormData();
+    formData.append('file', blobInfo.blob());
+    xhr.send(formData);
+  }
 }
 export default
 {
