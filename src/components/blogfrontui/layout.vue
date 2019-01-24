6f13f7b140ee5e2efd95e7fe72a9df845eb233ff
jiangqi <jiangqi@git.com.cn>
2019-01-24 16:45:29 +0800

    首页样式

--- src/components/blogfrontui/layout.vue
+++ src/components/blogfrontui/layout.vue
@@ -1,125 +1,192 @@
 <template>
     <div class="layout">
         <Layout>
-            <Header :style="{padding: '0 20px'}"> 
-                <Menu mode="horizontal" :theme="theme1" active-name="1">
+            <Header :style="{position: 'fixed', width: '100%'}">
+                <Menu mode="horizontal" theme="light" active-name="1">
                     <div class="layout-logo"></div>
                     <div class="layout-nav">
-                        <Submenu name="2">
-                            <template slot="title">
-                                <Icon type="ios-stats" />
-                                内容管理
-                            </template>
-                                <MenuItem name="Articlelist" to="Articlelist">文章管理</MenuItem>
-                                <MenuItem name="Essaylist" to="Essaylist">随笔管理</MenuItem>
-                                <MenuItem name="Albumlist" to="Albumlist">相册管理</MenuItem>
-                                <MenuItem name="Resourcelist" to="Resourcelist">资源贴管理</MenuItem>
-                                <MenuItem name="Comment" to="Comment">评论管理</MenuItem>
-                                <MenuItem name="Category" to="Category">分类管理</MenuItem>
-                        </Submenu>                       
-                        <Submenu name="3">
-                            <template slot="title">
-                                <Icon type="ios-stats" />
-                                系统管理
-                            </template>
-                                <MenuItem name="Log" to="Log">系统日志</MenuItem>
-                                <MenuItem name="Dic" to="Dic">数据字典</MenuItem>
-                                <MenuItem name="Config" to="Config">系统配置</MenuItem>
-                                <MenuItem name="User" to="User">用户管理</MenuItem>
-                                <MenuItem name="UserInfo" to="UserInfo">个人信息</MenuItem>
-                                <MenuItem name="Role" to="Role">角色管理</MenuItem>
-                                <MenuItem name="SysMenu" to="SysMenu">菜单管理</MenuItem>
-                        </Submenu>
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            首页
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            文章
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            随笔
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                            相册
+                        </MenuItem>
+                        <MenuItem name="5">
+                            <Icon type="ios-paper"></Icon>
+                            资源
+                        </MenuItem>
                     </div>
-                    <ul class="header-menu">
-                      <li class="header-menu-item">
-                      <Icon type="power"></Icon> <span v-on:click="logout">退出</span></li>
-                      <li class="header-menu-item">
-                      <Icon type="ios-person"></Icon> <span v-on:click="userInfo">{{ username }}</span></li>
-                    </ul>
+                    <div class="layout-search">
+                      <AutoComplete v-model="searchValue"  placeholder="请输入关键字" style="width:200px"></AutoComplete>
+                    </div>
                 </Menu>
             </Header>
-            <Content :style="{padding: '0 20px'}" style="min-height: 600px;">
-              <transition name="slide-fade">
-                <router-view/>
-              </transition>
+            <Content :style="{margin: '70px 20px 0', background: '#fff', minHeight: '500px'}">
+                <Row>
+                    <Col span="19" class="content-left">
+                      <ul class="article-list list-unstyled clearfix">
+                        <li class="article-item" v-for="item in contentData" :key="item.id"> 
+                          <div>
+                            <p class="list-top">
+                              <Time :time="item.createTime" />
+                            </p>
+                            <h4 class="title"><a href="/" class="link-title">
+                              {{item.title}}</a>
+                            </h4>
+                            <div class="list-footer">
+                              <span>阅读 {{ item.clicks ? item.clicks : 0 }}</span>
+                              <span> · 评论 53</span>
+                              <span> · 喜欢 17</span>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                      <!-- <div class="load-more" v-show="showMoreButtonFlag">
+                        <button class="ladda-button" @click="showmorelist"><span class="ladda-label">点击查看更多</span></button>
+                      </div> -->
+                    </Col>
+                    <Col span="5">
+                    
+                    </Col>
+                </Row>
+                <BackTop height=100></BackTop>
             </Content>
-            <Footer class="layout-footer-center">&copy; blogBack 2018.12</Footer>
+            <Footer class="layout-footer-center">2019 &copy; blog qi</Footer>
         </Layout>
     </div>
 </template>
 <script>
     export default {
-        data () {
-            return {
-                theme1: 'light',
-                userId: '',
-                menulist: [],
-                mainlist: [],
-                functionlist: [],
-                username: '',
-            }
+      data () {
+       return{
+         pageSize: 12,
+         contentData: [],
+         searchValue: '',
+         showMoreButtonFlag: true,
+       }
+      },
+      created(){
+        this.initlist();
+      },
+      methods: {
+        initlist() {
+          this.$http.get('/blog/tabArticle/list?pageNum=1&pageSize=' + this.pageSize).then(function(res){
+              this.contentData = res.data.data.list;
+          });
         },
-        created(){
-          
-        },        
-        methods: {
-          
+        showmorelist() {
+          this.pageSize = parseInt(this.pageSize) + 2;
+          this.$http.get('/blog/tabArticle/list?pageNum=1&pageSize=' + this.pageSize).then(function(res){
+              this.contentData = res.data.data.list;
+              if (this.pageSize == res.data.data.total) {
+                this.showMoreButtonFlag =false;
+              }
+          });
         }
+      }
     }
 </script>
 <style scoped>
-.layout{
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-}
-.ivu-layout-header{
-    background: #f5f7f9;
-}
-.layout-logo{
-    width: 100px;
-    height: 30px;
-    /* background: #5b6270; */
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
-}
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .ivu-layout-header{
+      background: #f5f7f9;
+      padding: 0 20px;
+      z-index: 10;
+    }
+    .layout-logo{
+        background: url(../../assets/logo.png) no-repeat left;
+        background-size: 100%;
+        width: 120px;
+        height: 40px;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        min-width: 500px;
+        margin: 0 auto;
+        float: center;
+    }
+    .layout-search{
+        float: right;
+        margin-right: 80px;
+    }
+    .layout-footer-center{
+        text-align: center;
+        
+    }
+    .content-left{
+        border-right: 1px dashed #eee;
+        min-height: 500px;
+        margin-left: 30px;
+    }
 
-.header-menu .header-menu-item {
-	list-style: none;
-    outline: 0;
-	float: right;
-	padding: 0 10px;
-	position: relative;
-	cursor: pointer;
-	z-index: 9;
-	-webkit-transition: all .2s ease-in-out;
-	transition: all .2s ease-in-out;
-	height: 60px;
-	font-size: 14px;
-}
-.header-menu-item:hover{color: #5cadff;}
-.layout-nav{
-    width: auto;
-    margin: 0 auto;
-}
-.layout-footer-center{
-    text-align: center;
-}
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
+
+
+    .article-list {
+        margin-top: 35px;
+    }
+    .list-unstyled {
+        padding-left: 0;
+        list-style: none;
+    }
+    .article-list .article-item {
+        display: list-item;
+        float: left;
+        width: 100%;
+        padding-right: 2px;
+        padding-bottom: 10px;
+        margin-bottom: 10px;
+        border-bottom-width: 1px;
+        border-bottom-style: dashed;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        word-wrap: break-word;
+        line-height: 1.5em;
+        border-color: #d9d9d9;
+    }
+    .article-list .list-top {
+        margin: 8px 0;
+        font-size: 12px;
+    }
+    .article-list .title {
+        margin-right: 10px;
+        font-size: 18px;
+        font-weight: bold;
+        line-height: 2.5;
+    }
+    .link-title {
+        color: #555;
+    }
+    .link-title:hover{
+        color: #bdc3c9;
+    }
+    .article-list .list-footer {
+        font-size: 12px;
+        line-height: 2.5;
+    }
+    .article-list .title {
+        font-size: 18px;
+        font-weight: bold;
+        line-height: 1.5;
+    }
 </style>
